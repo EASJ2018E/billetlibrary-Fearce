d180eb9bd45a1e5f0912ba00db8a0f3dae3351d5
Fearce <thekev93@gmail.com>
2018-09-16 13:15:26 +0200

    opgave 3 implement MC

---
+++ BilletLibrary/MC.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilletLibrary
+{
+    public class MC
+    {
+        /// <summary>
+        /// Property til nummerplade
+        /// </summary>
+        public string Nummerplade { get; set; }
+
+        /// <summary>
+        /// Property til dato
+        /// </summary>
+        public DateTime Dato { get; set; }
+
+
+        /// <summary>
+        /// MC pris
+        /// </summary>
+        /// <returns>Pris som decimal</returns>
+        public decimal Pris()
+        {
+            return 125;
+        }
+
+        /// <summary>
+        /// Køretøj type
+        /// </summary>
+        /// <returns>Køretøj som string</returns>
+        public string Køretøj()
+        {
+            return "MC";
+        }
+
+    }
+}
---
+++ BilletLibraryTests/MCTests.cs
@@ -0,0 +1,31 @@
+﻿using BilletLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BilletLibraryTests
+{
+    [TestClass]
+    public class MCTests
+    {
+        [TestMethod]
+        public void MCPrisTest()
+        {
+            //Arrange
+            MC mc = new MC();
+            //Act
+            decimal pris = 125;
+            //Assert
+            Assert.AreEqual(mc.Pris(), pris);
+        }
+
+        [TestMethod]
+        public void MCKøretøjTest()
+        {
+            //Arrange
+            MC mc = new MC();
+            //Act
+            string køretøj = "MC";
+            //Assert
+            Assert.AreEqual(mc.Køretøj(), køretøj);
+        }
+    }
+}
